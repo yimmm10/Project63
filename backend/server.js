60c456a599738213e79a56343b87e42c3c20873c
yimmm10 <yimaries10@gmail.com>
2025-01-09 01:46:15 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth'); // อ้างอิงไฟล์ auth.js ใน routes
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB(successfully)'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
